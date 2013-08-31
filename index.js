57c498a305e25094e2f1831eaedd5c13754c82b9
Cameron Bytheway <cameron@theflokk.com>
2013-08-30 18:46:40 -0600

    switch to single quotes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /**
  * Module dependencies
  */
-var url = require("url")
-  , resolve = require("path").resolve
-  , join = require("path").join
-  , debug = require("debug")("connect-base");
+var url = require('url')
+  , resolve = require('path').resolve
+  , join = require('path').join
+  , debug = require('debug')('connect-base');
 
 module.exports = function(options) {
   options = options || {};
@@ -16,38 +16,38 @@ module.exports = function(options) {
     , protoHeader = options.proto || 'x-forwarded-proto';
   
   return function base(req, res, next) {
-    var hostParts = req.headers.host.split(":");
+    var hostParts = req.headers.host.split(':');
 
     // Construct the base
     var base = {
-      protocol: (req.headers[protoHeader] || req.protocol || "http").split(",")[0],
-      hostname: (req.headers[hostHeader] || hostParts[0] || "").split(",")[0],
-      port: (req.headers[portHeader] || hostParts[1] || "").split(",")[0],
-      pathname: (req.headers[pathHeader] || "").split(",")[0]
+      protocol: (req.headers[protoHeader] || req.protocol || 'http').split(',')[0],
+      hostname: (req.headers[hostHeader] || hostParts[0] || '').split(',')[0],
+      port: (req.headers[portHeader] || hostParts[1] || '').split(',')[0],
+      pathname: (req.headers[pathHeader] || '').split(',')[0]
     };
 
     // Remove standard ports
-    if((base.port == 80 && base.protocol === "http") ||
-       (base.port == 443 && base.protocol === "https")) delete base.port;
+    if((base.port == 80 && base.protocol === 'http') ||
+       (base.port == 443 && base.protocol === 'https')) delete base.port;
 
-    debug("", base);
+    debug('', base);
 
     // Expose req.base
     req.base = url.format(base);
 
     // Expose req.resolve
     req.resolve = function() {
-      var path = Array.prototype.join.call(arguments, "/");
+      var path = Array.prototype.join.call(arguments, '/');
 
       // It's an absolue path
       if(url.parse(path).protocol) return path;
 
       // Resolve it relative to the mounted app
-      var urlLength = req.url.length === 1 ? req.originalUrl.length : -req.url.length+1
-        , appPath = !!req.originalUrl && req.url !== req.originalUrl ? req.originalUrl.slice(0,urlLength) : "/";
+      var urlLength = req.url.length === 1 ? req.originalUrl.length : -req.url.length + 1
+        , appPath = !!req.originalUrl && req.url !== req.originalUrl ? req.originalUrl.slice(0, urlLength) : '/';
 
       // Join the base path with the app path
-      var joinedPath = join(base.pathname || "/", appPath);
+      var joinedPath = join(base.pathname || '/', appPath);
 
       // It's a relative path
       var resolvedPath = url.format({
@@ -57,7 +57,7 @@ module.exports = function(options) {
         pathname: resolve(joinedPath, path)
       });
 
-      debug("Resolving path",joinedPath,"+",path,"->",resolvedPath);
+      debug('Resolving path', joinedPath, '+', path, '->', resolvedPath);
 
       return resolvedPath;
     };
