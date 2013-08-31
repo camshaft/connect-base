c102be1e87ed64cb4198205b7016b25ae93b677b
Cameron Bytheway <cameron@theflokk.com>
2013-08-30 18:49:07 -0600

    remove trailing slashes

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ module.exports = function(options) {
     if((base.port == 80 && base.protocol === 'http') ||
        (base.port == 443 && base.protocol === 'https')) delete base.port;
 
+    // Remove trailing slashes
+    if (base.pathname[base.pathname.length - 1] === '/') base.pathname = base.pathname.slice(0, base.pathname.length);
+
     debug('', base);
 
     // Expose req.base
